791c9301b675e66b265f8e6f51cf29d587aee200
MeerabFatima815 <meerabikram815@gmail.com>
2026-06-17 07:29:05 +0500

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ app = FastAPI(title="Bus Management System API", version="1.0.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
